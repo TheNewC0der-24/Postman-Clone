2af8da72cf4539aa19b368557da20407bc41638a
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-07-27 00:34:57 +0530

    Add loader

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -112,3 +112,11 @@ function updateResponseHeaders(headers) {
         responseHeadersContainer.append(valueElement);
     });
 };
+
+window.addEventListener('load', () => {
+    const loader = document.getElementById("loader");
+
+    setTimeout(() => {
+        loader.style.display = "none";
+    }, 5000);
+});
