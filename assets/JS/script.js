2d3429531f80ea88731a584a2a0133819f0e05d6
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-07-26 22:56:52 +0530

    ✨ feat: show response headers and details

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,11 +3,13 @@ console.log('Welcome to PostMan Clone');
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import prettyBytes from 'pretty-bytes';
 
 const queryParamsContainer = document.querySelector('[data-query-params]');
 const requestHeadersContainer = document.querySelector('[data-request-headers]');
 const keyValueTemplate = document.querySelector('[data-key-value-template]');
 const form = document.querySelector('[data-form]');
+const responseHeadersContainer = document.querySelector('[data-response-headers]');
 
 document.querySelector('[data-add-query-param-btn]').addEventListener('click', () => {
     queryParamsContainer.append(createKeyValuePair());
@@ -20,6 +22,23 @@ document.querySelector('[data-request-header-btn]').addEventListener('click', ()
 queryParamsContainer.append(createKeyValuePair());
 requestHeadersContainer.append(createKeyValuePair());
 
+axios.interceptors.request.use(request => {
+    request.customData = request.customData || {}
+    request.customData.startTime = new Date().getTime()
+    return request
+})
+
+function updateEndTime(response) {
+    response.customData = response.customData || {}
+    response.customData.time =
+        new Date().getTime() - response.config.customData.startTime
+    return response
+}
+
+axios.interceptors.response.use(updateEndTime, e => {
+    return Promise.reject(updateEndTime(e.response))
+})
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -28,11 +47,14 @@ form.addEventListener('submit', (e) => {
         method: document.querySelector('[data-method]').value,
         params: keyValuePairsToObjects(queryParamsContainer),
         headers: keyValuePairsToObjects(requestHeadersContainer)
-    }).then((res) => {
-        console.log(res);
-    }).catch((err) => {
-        console.log(err);
-    });
+    })
+        .catch(e => e)
+        .then((response) => {
+            document.querySelector('[data-response-section]').classList.remove('d-none');
+            updateResponseDetails(response);
+            // updateResponseEditor(response.data);
+            updateResponseHeaders(response.headers);
+        });
 });
 
 function createKeyValuePair() {
@@ -52,4 +74,26 @@ function keyValuePairsToObjects(container) {
         if (key === '') return data;
         return { ...data, [key]: value };
     }, {});
+};
+
+function updateResponseDetails(response) {
+    document.querySelector('[data-status]').textContent = response.status;
+    document.querySelector('[data-time]').textContent = response.customData.time;
+    document.querySelector('[data-size]').textContent = prettyBytes(
+        JSON.stringify(response.data).length + JSON.stringify(response.headers).length
+    );
+};
+
+function updateResponseHeaders(headers) {
+    responseHeadersContainer.innerHTML = '';
+    Object.entries(headers).forEach(([key, value]) => {
+        const keyElement = document.createElement('div');
+        keyElement.classList.add('fw-bold');
+        keyElement.textContent = key + ':';
+        responseHeadersContainer.append(keyElement);
+
+        const valueElement = document.createElement('div');
+        valueElement.textContent = value;
+        responseHeadersContainer.append(valueElement);
+    });
 };
